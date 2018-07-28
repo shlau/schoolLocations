7b515ead1ba9c028f746c21a6a25ff9c3f497bb5
BuildTools <unconfigured@null.spigotmc.org>
2018-07-28 15:45:40 -0700

    replace special characters in search field

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import requests
 from pandas import ExcelFile
 from apikeys import API_KEY
+import urllib
 
 """ load excel file """
 filename = "Schools.xlsx"
@@ -17,12 +18,16 @@ for i in range(len(schools)):
 
 """ places API call to get location of schools """
 search = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%s&types=establishment&location=32.7153300,-117.1572600&radius=500&key=%s"
+searchText = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address&key=%s"
 locations = []
 for query in searchQueries:
     print("query is : %s" % query)
-    formatted = search % (query, API_KEY)
+    encodedQuery = urllib.parse.quote(query)
+    formatted = searchText % (encodedQuery, API_KEY)
     print("formatted is : %s" % formatted) 
     result = requests.get(formatted)
     parsed_json = json.loads(result.content)
-    print(parsed_json['predictions'][0]['structured_formatting']['secondary_text'])
+    print(parsed_json['candidates'][0]['formatted_address'])
+"""     print(parsed_json['predictions'][0]['structured_formatting']['secondary_text']) """
+
 
