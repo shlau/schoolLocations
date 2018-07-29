b2ef6d09ec09d9038589af77b831425394f2c750
BuildTools <unconfigured@null.spigotmc.org>
2018-07-28 22:51:10 -0700

    write to excel file instead of txt, add dependencies file to run program

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 import requests
-from pandas import ExcelFile
+from pandas import ExcelFile, ExcelWriter
 from apikeys import API_KEY
 import urllib
 
@@ -16,21 +16,24 @@ districts = df['District']
 for i in range(len(schools)):
     searchQueries.append(schools[i] + ' ' + districts[i])
 
+locations = []
 """ places API call to get location of schools """
 searchText = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=formatted_address&key=%s"
-f = open("addresses.txt","w+")
+""" f = open("addresses.txt","w+") """
 for query in searchQueries:
-    """     print("query is : %s" % query) """
     encodedQuery = urllib.parse.quote(query)
     formatted = searchText % (encodedQuery, API_KEY)
-    """     print("formatted is : %s" % formatted)   """
     result = requests.get(formatted)
     parsed_json = json.loads(result.content)
-    """     print(parsed_json['candidates'][0]['formatted_address']) """
-    """     print(parsed_json['predictions'][0]['structured_formatting']['secondary_text']) """
+
     if (parsed_json['status'] == 'OK'):
         address = parsed_json['candidates'][0]['formatted_address']
     else:
         address = 'NO ADDRESS FOUND FOR %s' % query
-    f.write("%s\n" % address)
-f.close()
+    locations.append(address)
+"""     f.write("%s\n" % address) """
+""" f.close() """
+addressDF = pd.DataFrame({'title':searchQueries,'address':locations })
+writer = ExcelWriter('maptemplate.xlsx')
+addressDF.to_excel(writer,'Sheet1',index=False)
+writer.save()
